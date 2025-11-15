273db433f0a3eb766080fcb01b44a07ccf691449
Hayden <haydenseymour02@gmail.com>
2025-11-15 14:05:41 +1100

    Update Tools.jsx

--- src/pages/Tools.jsx
+++ src/pages/Tools.jsx
@@ -1,5 +1,4 @@
 import { useDecodingText } from '../hooks/useDecodingText'
-import ResourceTile from '../components/ResourceTile'
 import './Page.css'
 import './Tools.css'
 
@@ -10,12 +9,7 @@ function Tools() {
     <div className="page">
       <h1 className="page-title">{displayText}</h1>
       <div className="tools-grid">
-        <ResourceTile 
-          title="University Program Planning" 
-          to="/tools/university-program-planning"
-        >
-          Plan your university program based on your preferences. Select a program to view core subjects and details.
-        </ResourceTile>
+        {/* Tools will be added here */}
       </div>
     </div>
   )
